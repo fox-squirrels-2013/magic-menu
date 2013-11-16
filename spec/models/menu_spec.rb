6f2e60ebc4cfea410114179b61dfcafd2a657c28
TGOlson <tydotg@gmail.com>
2013-11-16 15:48:33 -0800

    new test to see if a menu can be created

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe Menu do
 
-  describe "validations" do
-    it { should validate_presence_of :name }
-  end
+		it "can create a new menu" do
+			lindeman = Menu.create!(name: "Test")
+			expect(Menu.last.name).to eq('Test')
+		end
+
+	describe "validations" do
+		it { should validate_presence_of :name }
+	end
 
 end
 
