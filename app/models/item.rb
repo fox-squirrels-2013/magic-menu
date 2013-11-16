f83ba52f2006f5d6bf812e6b9c8e598f067ea077
Sherif Abushadi <sherif@amgando.org>
2013-11-16 14:46:25 -0800

    big one. add new item, css cleanup, add price_as_money method to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ActiveRecord::Base
 	has_many :menu_items
 	has_many :menus, through: :menu_items
+
+  # convert cents to dollars and prepend USD $ symbol
+  def price_as_money
+    "$%.2f" % (self.price / 100.0)
+  end
 end
