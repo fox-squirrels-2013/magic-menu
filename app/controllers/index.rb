8c86e7f5f16640f68c2071a4a3cb209fe28379e5
Apprentice <apprentice@dbc-01.local>
2013-11-16 15:54:19 -0800

    Added RESTful post route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,32 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/:id' do
+  # Look in app/views/index.erb
+  erb :index
+end
+
+post '/' do
+  p params
+  p Menu.create(params)
+  erb :index
+end
+
+update '/:id' do
+end
+
+delete '/:id' do
+end
+
+get '/item' do
+end
+
+post '/item' do
+end
+
+update '/item/:id' do
+end
+
+delete '/item/:id' do
+end
