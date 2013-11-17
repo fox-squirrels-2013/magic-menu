f49dfdcc2ed32da3e50e0357bc216f576014056f
TGOlson <tydotg@gmail.com>
2013-11-16 19:58:24 -0800

    refactored first menu page test, and created new test to validate viewing all menu items

--- spec/controllers/menu_page_spec.rb
+++ spec/controllers/menu_page_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 feature "Menu Page" do
 
-  scenario "Guest may see all menu items" do
+  scenario "Guest may see a specific menu page" do
+  	m = Menu.create name: 'Tester Menu'
+    visit "/menus/#{m.id}"
+    expect(page).to have_content("Tester Menu")
+  end
+
+  scenario "Guest may see items that belong to a menu" do
   	m = Menu.create name: 'Tester'
+  	m.items << Item.create({name: 'test item', price: 123})
     visit '/menus/' + "#{m.id}"
-    expect(page).to have_content("Items")
+    expect(page).to have_content("test item")
   end
 
 
