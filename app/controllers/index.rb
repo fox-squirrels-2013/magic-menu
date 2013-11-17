0b4f931bcd864e28609191ffb1c911486f3425d6
Kenneth Yu <kenyu@ucdavis.edu>
2013-11-16 21:46:09 -0800

    Holy shit, this is awesome. I've got menus, items, lists of menus on the homepage, etc.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,5 +4,27 @@ get '/' do
 end
 
 get '/menus/new' do
+  erb :new
+end
 
+post '/menus/new' do
+  @menu = Menu.create!(name: params[:menu_name])
+  redirect "/menus/#{@menu.id}"
+end
+
+get '/menus/:id' do
+  # params[:id]
+  erb :menu_id
+end
+
+post '/menus/:id' do
+  # p params
+  # p params[:id]
+  # p params["id"]
+  p "Inside menus/1 post route."
+  erb :menu_id
+end
+
+get '/items' do
+  erb :items
 end
