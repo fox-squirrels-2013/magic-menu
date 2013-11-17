75601d0f81daa15868bf725053c9c6dab65f92c7
Kenneth Yu <kenyu@ucdavis.edu>
2013-11-16 23:08:37 -0800

    Can add menus and items and the lists show up, woohoo.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,8 +8,9 @@ get '/menus/new' do
 end
 
 post '/menus/new' do
-  @menu = Menu.create!(name: params[:menu_name])
-  redirect "/menus/#{@menu.id}"
+  @menu = Menu.create(name: params[:menu_name])
+  erb :new
+  # redirect "/menus/#{@menu.id}"
 end
 
 get '/menus/:id' do
@@ -18,13 +19,21 @@ get '/menus/:id' do
 end
 
 post '/menus/:id' do
-  # p params
-  # p params[:id]
-  # p params["id"]
   p "Inside menus/1 post route."
-  erb :menu_id
+  @item = Item.find_by_name(params[:menu_items])
+  @items_menu = ItemsMenu.create(menu_id: params[:id], item_id: @item.id, price: params[:price])
+
+  # Why does the below not work?
+  # p @ItemsMenu.all
+  # make price match up!
+  erb :menu
 end
 
 get '/items' do
+  erb :items
+end
+
+post '/items' do
+  @item = Item.create(name: params[:item_name])
   erb :items
 end
--- app/models/items_menu.rb
+++ app/models/items_menu.rb
@@ -1,5 +1,5 @@
 class ItemsMenu < ActiveRecord::Base
-  validates :price, presence: true, numericality: true
+  validates :price, presence: true, numericality: true, uniqueness: true
   belongs_to :menu
   belongs_to :item
 end
