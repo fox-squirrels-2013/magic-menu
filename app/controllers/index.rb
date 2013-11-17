014d9417b6c84e6523635e3b61240487ca5b232d
Kenneth Yu <kenyu@ucdavis.edu>
2013-11-17 00:41:50 -0800

    Got the items on the menu_id erb page to only show the items and prices for that particular menu, woohoo.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,4 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
@@ -10,23 +9,19 @@ end
 post '/menus/new' do
   @menu = Menu.create(name: params[:menu_name])
   erb :new
-  # redirect "/menus/#{@menu.id}"
+  redirect "/menus/#{@menu.id}"
 end
 
 get '/menus/:id' do
-  # params[:id]
   erb :menu_id
 end
 
 post '/menus/:id' do
-  p "Inside menus/1 post route."
   @item = Item.find_by_name(params[:menu_items])
   @items_menu = ItemsMenu.create(menu_id: params[:id], item_id: @item.id, price: params[:price])
-
   # Why does the below not work?
   # p @ItemsMenu.all
-  # make price match up!
-  erb :menu
+  erb :menu_id
 end
 
 get '/items' do
--- app/models/items_menu.rb
+++ app/models/items_menu.rb
@@ -1,5 +1,6 @@
 class ItemsMenu < ActiveRecord::Base
-  validates :price, presence: true, numericality: true, uniqueness: true
+  validates :price, presence: true, numericality: true
+  validates :item_id, uniqueness: { scope: :menu_id, message: "The same item can only appear once per menu!!!" }
   belongs_to :menu
   belongs_to :item
 end
