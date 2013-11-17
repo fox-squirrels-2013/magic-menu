1becd7aeb236cdf6fb1179c63e0152ddea8d1f06
TGOlson <tydotg@gmail.com>
2013-11-16 19:20:10 -0800

    cleaning up comments

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ActiveRecord::Base
   	validates_presence_of :name
   	validates_presence_of :price
-  	validates_format_of :price, with: /\d{3,}/, message: 'Integer format only'
+  	validates_format_of   :price, with: /\d{3,}/, message: 'Integer format only'
 
-  	# belongs_to :menu
     has_and_belongs_to_many :menus
 end
