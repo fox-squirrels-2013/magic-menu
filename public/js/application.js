7c58eb8fb9d03aaa7108013e6ce1e9adc0c19781
TGOlson <tydotg@gmail.com>
2013-11-16 21:43:00 -0800

    refactor ajax request so other methods can use same function

--- public/js/application.js
+++ public/js/application.js
@@ -1,45 +1,22 @@
 $(document).ready(function() {
 
-  $('#item_select').on('change', function(e){
-  	console.log(this.value.split(' '))
-  	var values = this.value.split(' ')
-
-  	$.ajax({
-  		type: 'POST',
-  		url: '/menus/' + values[0] + '/additem/' + values[1]
-  	}).done(function(server_data){
-  		console.log('creating new page')
-  		$('body').html(server_data)
-  	}).fail(function(jqXHR, textStatus, errorThrown){
-  		console.log("fail" + errorThrown)
-  	})
-  })
+	$('#item_select').on('change', function(e){
+		var values = this.value.split(' ')
+		url = '/menus/' + values[0] + '/additem/' + values[1]
+		ajaxReq(url, 'POST')
+	})
 
 });
 
-
-  // function ajaxReq(url, type, data){
-  //   // data can be undefined
-  //   $.ajax({
-  //     url: url,
-  //     type: type,
-  //     data: data
-  //   }).done(function(server_data){
-  //     $('body').html(server_data)
-  //   }).fail(function(jqXHR, textStatus, errorThrown){
-  //     console.log("fail" + errorThrown)
-  //   })
-  // }
-
-// $.post( "ajax/test.html", function( data ) {
-//   $( ".result" ).html( data );
-// });
-
-
-// $.ajax({
-//   type: "POST",
-//   url: url,
-//   data: data,
-//   success: success,
-//   dataType: dataType
-// });
+function ajaxReq(url, type, data){
+    // data can be undefined
+    $.ajax({
+    	url: url,
+    	type: type,
+    	data: data
+    }).done(function(server_data){
+    	$('body').html(server_data)
+    }).fail(function(jqXHR, textStatus, errorThrown){
+    	console.log("fail" + errorThrown)
+    })
+}
